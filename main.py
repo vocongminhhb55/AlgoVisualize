6c2cbf16478dc4eb09120e75627ef5657ee99435
Cong Minh Vo <vocongminhhb55@gmail.com>
2022-03-07 02:22:43 +0700

    Hopefully done

--- main.py
+++ main.py
@@ -6,15 +6,11 @@ WIDTH = 800
 PEN = turtle.Turtle()
 
 RED = '#ff0000'
-BLUE = '#0000ff'
 BLACK = '#000000'
 GREEN = '#00ff00'
-YELLOW = '#ffff00'
 WHITE = '#ffffff'
-PURPLE = '#800080'
 ORANGE = '#ffa500'
 GREY = '#808080'
-TURQUOISE = '#40e0d0'
 
 
 def draw_rect(pen, color, x, y, width, height):
@@ -23,8 +19,8 @@ def draw_rect(pen, color, x, y, width, height):
     pen.pu()
     pen.goto(x, y)
     pen.pd()
-    for _ in range(4):
-        if _ % 2 == 0:
+    for side in range(4):
+        if side % 2 == 0:
             pen.forward(width)
             pen.left(90)
 
@@ -66,8 +62,8 @@ def draw_grid(pen, rows, cols, width):
     gap = find_gap(rows, cols, width)
     for i in range(rows + 1):
         draw_line(pen, GREY, (0, i * gap), (cols * gap, i * gap))
-    for j in range(cols + 1):
-        draw_line(pen, GREY, (j * gap, 0), (j * gap, rows * gap))
+    for i in range(cols + 1):
+        draw_line(pen, GREY, (i * gap, 0), (i * gap, rows * gap))
 
 
 class Spot:
@@ -88,12 +84,6 @@ class Spot:
     def get_pos(self):
         return self.row, self.col
 
-    def is_closed(self):
-        return self.color == RED
-
-    def is_open(self):
-        return self.color == GREEN
-
     def is_barrier(self):
         return self.color == BLACK
 
@@ -101,28 +91,19 @@ class Spot:
         return self.color == ORANGE
 
     def is_end(self):
-        return self.color == TURQUOISE
-
-    def reset(self):
-        self.color = WHITE
+        return self.color == RED
 
     def make_start(self):
         self.color = ORANGE
-
-    def make_closed(self):
-        self.color = RED
-
-    def make_open(self):
-        self.color = GREEN
 
     def make_barrier(self):
         self.color = BLACK
 
     def make_end(self):
-        self.color = TURQUOISE
+        self.color = RED
 
     def make_path(self):
-        self.color = PURPLE
+        self.color = GREEN
 
     def draw(self, pen):
         if self.color != WHITE:
@@ -131,20 +112,17 @@ class Spot:
     def update_neighbors(self, grid):
         self.neighbors = []
 
-        if self.row < self.total_cols - 1 and not grid[self.row + 1][self.col].is_barrier():  # DOWN
-            self.neighbors.append(grid[self.row + 1][self.col])
-
-        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():  # UP
-            self.neighbors.append(grid[self.row - 1][self.col])
-
-        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():  # RIGHT
+        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():
             self.neighbors.append(grid[self.row][self.col + 1])
 
-        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():  # LEFT
+        if self.row < self.total_cols - 1 and not grid[self.row + 1][self.col].is_barrier():
+            self.neighbors.append(grid[self.row + 1][self.col])
+
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():
             self.neighbors.append(grid[self.row][self.col - 1])
 
-    def __lt__(self, other):
-        return False
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():
+            self.neighbors.append(grid[self.row - 1][self.col])
 
 
 def make_grid(rows, cols, width):
@@ -160,9 +138,9 @@ def make_grid(rows, cols, width):
 
 
 def make_grid_border(grid, rows, cols):
-    for i in range(0, rows):
+    for i in range(rows):
         if i == 0 or i == rows - 1:
-            for j in range(0, cols):
+            for j in range(cols):
                 grid[j][i].make_barrier()
         else:
             grid[0][i].make_barrier()
@@ -180,6 +158,7 @@ def draw(pen, grid, rows, cols, width):
 
 
 def bresenham_line_gen(grid, start, end):
+    # roguebasin.com/index.php/Bresenham%27s_Line_Algorithm#Python
     # Setup initial conditions
     x1, y1 = start
     x2, y2 = end
@@ -231,10 +210,10 @@ def bresenham_line_gen(grid, start, end):
         grid[x][y].make_barrier()
 
 
-def input_data_from_file(filename):
+def get_data_from_file(filename):
     with open(filename) as f:
         array = []
-        for line in f:  # read rest of lines
+        for line in f:
             array.append([int(x) for x in line.split()])
         return array
 
@@ -255,54 +234,15 @@ def manhattan_distance(p1, p2):
 
 
 def reconstruct_path(came_from, current):
+    cost = 0
     while current in came_from:
         current = came_from[current]
         current.make_path()
+        cost += 1
+    return cost
 
 
-def breadth_first_search(grid, start, end):
-    count = 0
-    open_set = Queue()
-    open_set.put((0, count, start))
-    came_from = {}
-    g_score = {spot: float("inf") for row in grid for spot in row}
-    g_score[start] = 0
-    f_score = {spot: float("inf") for row in grid for spot in row}
-    f_score[start] = manhattan_distance(start.get_pos(), end.get_pos())
-
-    open_set_hash = {start}
-
-    while not open_set.empty():
-
-        current = open_set.get()[2]
-        open_set_hash.remove(current)
-
-        if current == end:
-            reconstruct_path(came_from, end)
-            start.make_start()
-            end.make_end()
-            return True, g_score[end]
-
-        for neighbor in current.neighbors:
-            temp_g_score = g_score[current] + 1
-
-            if temp_g_score < g_score[neighbor]:
-                came_from[neighbor] = current
-                g_score[neighbor] = temp_g_score
-                f_score[neighbor] = temp_g_score
-                if neighbor not in open_set_hash:
-                    count += 1
-                    open_set.put((f_score[neighbor], count, neighbor))
-                    open_set_hash.add(neighbor)
-                    neighbor.make_open()
-
-        if current != start:
-            current.make_closed()
-
-    return False, -1
-
-
-def uniform_cost_search(grid, start, end):
+def a_star_search(grid, start, end):
     count = 0
     open_set = PriorityQueue()
     open_set.put((0, count, start))
@@ -331,15 +271,11 @@ def uniform_cost_search(grid, start, end):
             if temp_g_score < g_score[neighbor]:
                 came_from[neighbor] = current
                 g_score[neighbor] = temp_g_score
-                f_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + manhattan_distance(neighbor.get_pos(), end.get_pos())
                 if neighbor not in open_set_hash:
                     count += 1
                     open_set.put((f_score[neighbor], count, neighbor))
                     open_set_hash.add(neighbor)
-                    neighbor.make_open()
-
-        if current != start:
-            current.make_closed()
 
     return False, -1
 
@@ -378,23 +314,18 @@ def greedy_best_first_search(grid, start, end):
                     count += 1
                     open_set.put((f_score[neighbor], count, neighbor))
                     open_set_hash.add(neighbor)
-                    neighbor.make_open()
-
-        if current != start:
-            current.make_closed()
 
     return False, -1
 
 
-def a_star_algorithm(grid, start, end):
+def uniform_cost_search(grid, start, end):
     count = 0
     open_set = PriorityQueue()
     open_set.put((0, count, start))
     came_from = {}
     g_score = {spot: float("inf") for row in grid for spot in row}
     g_score[start] = 0
-    f_score = {spot: float("inf") for row in grid for spot in row}
-    f_score[start] = manhattan_distance(start.get_pos(), end.get_pos())
+    # delete f_score because in this search f(n) = g(n)
 
     open_set_hash = {start}
 
@@ -415,21 +346,106 @@ def a_star_algorithm(grid, start, end):
             if temp_g_score < g_score[neighbor]:
                 came_from[neighbor] = current
                 g_score[neighbor] = temp_g_score
-                f_score[neighbor] = temp_g_score + manhattan_distance(neighbor.get_pos(), end.get_pos())
                 if neighbor not in open_set_hash:
                     count += 1
-                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set.put((g_score[neighbor], count, neighbor))
                     open_set_hash.add(neighbor)
-                    neighbor.make_open()
-
-        if current != start:
-            current.make_closed()
 
     return False, -1
 
 
+def breadth_first_search(start, end):
+    # delete count variable because now this use normal queue
+    open_set = Queue()
+    open_set.put(start)
+    came_from = {}
+
+    open_set_hash = {start}
+    visited = {start}
+
+    while not open_set.empty():
+
+        current = open_set.get()
+        open_set_hash.remove(current)
+
+        if current == end:
+            cost = reconstruct_path(came_from, end)
+            start.make_start()
+            end.make_end()
+            return True, cost
+
+        for neighbor in current.neighbors:
+            if neighbor not in open_set_hash and neighbor not in visited:
+                came_from[neighbor] = current
+                open_set.put(neighbor)
+                open_set_hash.add(neighbor)
+                visited.add(neighbor)
+
+    return False, -1
+
+
+def depth_limited_search(start, end, max_depth, path, visited):
+    if start == end:
+        return True, path
+
+    if max_depth <= 0:
+        return False, -1
+
+    for neighbor in start.neighbors:
+        if neighbor not in path and neighbor not in visited:
+            path[neighbor] = start
+            result = depth_limited_search(neighbor, end, max_depth - 1, path, visited)
+            if result[0]:
+                return True, path
+
+    path.pop(start, None)
+    visited.append(start)
+    return False, -1
+
+
+def iterative_deepening_search(start, end, max_depth):
+    for limit in range(max_depth):
+        path = {}
+        visited = []
+        result = depth_limited_search(start, end, limit, path, visited)
+        if result[0]:
+            cost = reconstruct_path(path, end)
+            start.make_start()
+            end.make_end()
+            return result[0], cost
+
+    return False, -1
+
+
+def menu():
+    menu_options = {
+        1: 'Breadth-first search',
+        2: 'Uniform-cost search',
+        3: 'Iterative deepening search',
+        4: 'Greedy-best first search',
+        5: 'Graph-search A* ',
+    }
+
+    def print_menu():
+        for key in menu_options.keys():
+            print(key, '--', menu_options[key])
+
+    while True:
+        print_menu()
+        option = ''
+        try:
+            option = int(input('Enter your choice: '))
+        except:
+            print('Wrong input. Please enter a number ...')
+        # Check what choice was entered and act accordingly
+        if option in range(1, 6):
+            return option
+        else:
+            print('Invalid option. Please enter a number between 1 and 5.')
+
+
 if __name__ == "__main__":
-    data = input_data_from_file('input.txt')
+    data = get_data_from_file('input.txt')
     cols, rows = data[0]
     x_start, y_start, x_goal, y_goal = data[1]
     cols += 1
@@ -441,7 +457,7 @@ if __name__ == "__main__":
     grid[x_start][y_start].make_start()
     grid[x_goal][y_goal].make_end()
     for i in range(3, data[2][0] + 3):
-        for j in range(0, int(len(data[i]) / 2)):
+        for j in range(int(len(data[i]) / 2)):
             point1 = data[i][j * 2], data[i][j * 2 + 1]
             if j != int(len(data[i]) / 2) - 1:
                 point2 = data[i][j * 2 + 2], data[i][j * 2 + 3]
@@ -453,7 +469,17 @@ if __name__ == "__main__":
         for spot in row:
             spot.update_neighbors(grid)
 
-    result = breadth_first_search(grid, grid[x_start][y_start], grid[x_goal][y_goal])
+    user_choice = menu()
+    if user_choice == 1:
+        result = breadth_first_search(grid[x_start][y_start], grid[x_goal][y_goal])
+    elif user_choice == 2:
+        result = uniform_cost_search(grid, grid[x_start][y_start], grid[x_goal][y_goal])
+    elif user_choice == 3:
+        result = iterative_deepening_search(grid[x_start][y_start], grid[x_goal][y_goal], 50)
+    elif user_choice == 4:
+        result = greedy_best_first_search(grid, grid[x_start][y_start], grid[x_goal][y_goal])
+    elif user_choice == 5:
+        result = a_star_search(grid, grid[x_start][y_start], grid[x_goal][y_goal])
 
     draw(PEN, grid, rows, cols, WIDTH)
 
